8d25f240b2b0a812e91fe7ae469390f6ccbaee97
Furkan Başaran <frknbasaran@gmail.com>
2024-07-15 23:37:36 +0300

    chore: tailwindcss setup.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'ember-tic-tac-toe/config/environment';
-
+import config from 'tic-tac-toe/config/environment';
+import 'tic-tac-toe/tailwind-setup.css';
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,5 +7,39 @@ module.exports = function (defaults) {
     // Add options here
   });
 
-  return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    staticEmberSource: true,
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      config: 'postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  });
 };
