4f05be3f519d23e245efc982e32c5761b663e157
Furkan Başaran <frknbasaran@gmail.com>
2024-07-16 11:32:14 +0300

    chore: change route mode to hash.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function (environment) {
     modulePrefix: 'ember-tic-tac-toe',
     environment,
     rootURL: '/',
-    locationType: 'history',
+    locationType: 'hash',
     EmberENV: {
       EXTEND_PROTOTYPES: false,
       FEATURES: {
